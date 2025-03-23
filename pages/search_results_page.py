fad4b9ff21d7561a69046aba221f294805ff4200
Jake Lee <jkjalee@gmail.com>
2025-03-22 22:04:37 -0700

    update

--- pages/search_results_page.py
+++ pages/search_results_page.py
@@ -6,8 +6,7 @@ class SearchResultsPage(Page):
     RESULTS_CATEGORY_NAME = (By.CSS_SELECTOR, "[data-test='lp-resultsCount'] > span")
 
     def verify_search_results(self, search_word):
-        results_category_name = self.find_element(self.RESULTS_CATEGORY_NAME)
-        assert search_word.lower() in results_category_name.text.lower(), 'Results category name not found'
+        self.verify_partial_text(search_word, self.RESULTS_CATEGORY_NAME)
 
     def verify_search_url(self, search_word):
-        assert search_word.lower() in self.driver.current_url.lower(), f'Expected query not in {self.driver.current_url.lower()}'
+        self.verify_partial_url(search_word)
