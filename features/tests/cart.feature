e18420cc771df256986c3d884cd51019ab2eb5cb
Jake Lee <jkjalee@gmail.com>
2025-03-22 22:09:49 -0700

    handle empty cart page

--- features/tests/cart.feature
+++ features/tests/cart.feature
@@ -1,12 +1,17 @@
 Feature: Add product into cart functionality
 
-  Scenario: User can add a product into cart
+#  Scenario: User can add a product into cart
+#    Given Open Target.com
+#    When Search for tea
+#    Then Verify search results shown for tea
+#    When Click on "Add to cart" button on a product
+#    Then Side navigation panel with "Add to cart" button is shown
+#    When Click on "Add to cart" button in the navigation panel
+#    And Click on the "View cart & check out" button in navigation panel
+#    Then Cart page is displayed with the added item and correct subtotal
+
+
+  Scenario: "Your cart is empty" message is shown for empty cart
     Given Open Target.com
-    When Input tea into search field
-    And Click on search icon
-    Then Product results for tea are shown
-    When Click on "Add to cart" button on a product
-    Then Right-side navigation panel with "Add to cart" button is shown
-    When Click on "Add to cart" button in the navigation panel
-    And Click on the "View cart & check out" button in navigation panel
-    Then Cart page is displayed with the added item and correct subtotal
+    When Click on Cart icon
+    Then Verify "Your cart is empty" message is shown
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,50 +1,56 @@
-from utils.wait_utils import WaitUtils
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support.wait import WebDriverWait
 
 class Page:
     def __init__(self, driver):
         self.driver = driver
         self.base_url = 'https://www.target.com/'
+        self.wait = WebDriverWait(self.driver, timeout=10)
 
     def open_url(self, url):
         self.driver.get(url)
 
-    def find_element(self, locator):
-        return WaitUtils.wait_for_element_to_be_visible(self.driver.wait, locator)
+    def find_element(self, *locator):
+        return self.driver.find_element(*locator)
 
     def find_elements(self, *locator):
-        self.driver.find_elements(*locator)
+        return self.driver.find_elements(*locator)
 
-    def click(self, locator):
-        WaitUtils.wait_for_element_to_be_clickable(self.driver.wait, locator).click()
+    def click(self, *locator):
+        self.driver.find_element(*locator).click()
 
-    def input_text(self, text, locator):
-        WaitUtils.wait_for_element_to_be_visible(self.driver.wait, locator).send_keys(text)
+    def input_text(self, text, *locator):
+        self.driver.find_element(*locator).send_keys(text)
 
-    # def wait_until_clickable(self, locator):
-    #     self.wait.until(
-    #         EC.element_to_be_clickable(locator),
-    #         message=f'Element not clickable by {locator}'
-    #     )
-    #
-    # def wait_until_clickable_click(self, locator):
-    #     self.wait.until(
-    #         EC.element_to_be_clickable(locator),
-    #         message=f'Element not clickable by {locator}'
-    #     ).click()
-    #
-    # def wait_until_visible(self, locator):
-    #     self.wait.until(
-    #         EC.visibility_of_element_located(locator),
-    #         message=f'Element not visible by {locator}'
-    #     )
-    #
-    # def wait_until_invisible(self, locator):
-    #     self.wait.until(
-    #         EC.invisibility_of_element_located(locator),
-    #         message=f'Element still visible by {locator}'
-    #     )
+    def wait_until_clickable(self, locator):
+        self.wait.until(
+            EC.element_to_be_clickable(locator),
+            message=f'Element not clickable by {locator}'
+        )
+
+    def wait_until_clickable_click(self, locator):
+        self.wait.until(
+            EC.element_to_be_clickable(locator),
+            message=f'Element not clickable by {locator}'
+        ).click()
+
+    def wait_until_visible(self, locator):
+        self.wait.until(
+            EC.visibility_of_element_located(locator),
+            message=f'Element not visible by {locator}'
+        )
+
+    def wait_until_are_visible(self, locator):
+        self.wait.until(
+            EC.visibility_of_all_elements_located(locator),
+            message=f'Elements not visible by {locator}'
+        )
+
+    def wait_until_invisible(self, locator):
+        self.wait.until(
+            EC.invisibility_of_element_located(locator),
+            message=f'Element still visible by {locator}'
+        )
 
     def verify_text(self, expected_text, *locator):
         actual_text = self.find_element(*locator).text
